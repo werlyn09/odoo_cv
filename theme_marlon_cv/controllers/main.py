d38ca8a20628149cca181d7b0f707160b2090929
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-30 03:35:20 +0100

    sx

--- theme_marlon_cv/controllers/main.py
+++ theme_marlon_cv/controllers/main.py
@@ -14,18 +14,18 @@ env = jinja2.Environment(loader=loader, autoescape=True)
 
 class MainController(http.Controller):
 
-    @http.route('/', methods=['GET'], auth='none')
-    def cv_endpoint(self, **kwargs):
-        my_current_url = request.session.get('my_current_url')
-        _logger.info(_('URL: %s') % (my_current_url))
-        _logger.info(_('URL: %s') % (request.session))
-        company = http.request.env['res.company'].sudo().search([], limit=1)
-        return env.get_template('index.html').render({
-            'company': company,
-            'company_fhone': company.phone,
-            'company_email': company.email,
-            'logo': company.logo and 'data:image/png;base64,%s' % company.logo.decode()
-        })
+    # @http.route('/', methods=['GET'], auth='none')
+    # def cv_endpoint(self, **kwargs):
+    #     my_current_url = request.session.get('my_current_url')
+    #     _logger.info(_('URL: %s') % (my_current_url))
+    #     _logger.info(_('URL: %s') % (request.session))
+    #     company = http.request.env['res.company'].sudo().search([], limit=1)
+    #     return env.get_template('index.html').render({
+    #         'company': company,
+    #         'company_fhone': company.phone,
+    #         'company_email': company.email,
+    #         'logo': company.logo and 'data:image/png;base64,%s' % company.logo.decode()
+    #     })
 
     @http.route('/modal', methods=['GET'], auth='public')
     def modal(self, **kwargs):
